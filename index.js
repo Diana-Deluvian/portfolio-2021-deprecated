6e84a1d0a84fd4fa2122ae4f0ebba979353752d4
Diana Deluvian <diana.deluvian@gmail.com>
2021-08-11 15:09:36 +0200

    made animations spawn randomly, added an ocean div for them to fall into

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const dripLocations =[13.85, 19.7, 22.1, 25.8, 28.5, 31.4, 34.2, 37.8,
                       44.6, 50.9, 56.35, 62.33, 68.53, 72.82, 79.17,
-                     81.9, 84.85];
+                      81.9, 84.85];
+
+                      
+function randomDropLocation(){
+   return Math.floor(Math.random()*dripLocations.length);
+}
 
 function addNewDrop(){
    const newDrop = document.createElement("div");
    newDrop.classList.add("drop");
+   newDrop.style.left = (dripLocations[randomDropLocation()] -0.5) + "vw";
    console.log(newDrop);
+   console.log(dripLocations[randomDropLocation()] + "vw");
    document.body.appendChild(newDrop);
+   setTimeout(() =>  {
+      newDrop.remove();
+      addNewDrop()}, Math.floor(Math.random()*6000 + 4000));
+      // 4000 is the duration of the animation
 }
+
+
+
+window.onload = function() {
+   addNewDrop();
+   
+ };
